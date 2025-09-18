3517bcdeacd1506b418e9944d0187a6e3a3fad51
Joe Jordan <dev@ignitesoft.com>
2025-09-17 20:00:37 -0400

    refactor: root index, app.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react';
-import { RouterProvider } from '@tanstack/react-router';
 import { createRoot } from 'react-dom/client';
 import '@/styles/app.css';
-import { ThemeProvider } from '@/contexts/theme-context';
-import { router } from './routes';
+import App from './app';
 
 createRoot(document.querySelector('#root')!).render(
   <StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <App />
   </StrictMode>,
 );
